f0286b782791c8b0825909a0613ddbdaa616d1d9
Michael Matich <matich.mike@gmail.com>
2020-03-31 00:30:08 +0000

    first array element

---
+++ head.js
@@ -0,0 +1,12 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else if (actual !== expected) {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+const head = function (actual) {
+  return actual[0];
+}
+assertEqual(head([5,6,7]), 5);
+assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
