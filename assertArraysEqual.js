d0b191fdee2c55f80f0894fee33b8df441783256
Michael Matich <matich.mike@gmail.com>
2020-04-01 01:24:52 +0000

    assertArraysEqual

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -9,16 +9,13 @@ const assertEqual = function(actual, expected) {
 const assertArraysEqual = function(firstArray, secondArray) {
   if (firstArray.length !== secondArray.length) {
     console.log(`${firstArray} !== ${secondArray}`);
-    return false;
   }
   for (let i = 0; i < firstArray.length; i++) {
     if (firstArray[i] !== secondArray[i]) {
       console.log(`${firstArray} !== ${secondArray}`);
-      return false;
     }
   }
   console.log(`${firstArray} === ${secondArray}`);
-  return true;
 }
 assertArraysEqual([1, 2, 3], [1, 2, 3]);
 assertArraysEqual([1, 2, 3], [3, 2, 1]);
