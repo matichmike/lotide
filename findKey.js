bd1fd3644c78c1bfef29047899ef0b58d4d1a5f6
Michael Matich <matich.mike@gmail.com>
2020-04-03 01:06:55 +0000

    find first key

---
+++ findKey.js
@@ -0,0 +1,29 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else if (actual !== expected) {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const findKey = function(object, callback) {
+  // should return the object's first key for which cb returns truthy
+  const categories = Object.keys(object);
+  for (let category of categories) {
+    if (callback(object[category])) {
+      return category;
+    }
+  }
+}
+
+let result = findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 2 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 3 }
+}, x => x.stars === 2) // => "noma"
+
+console.log(result);
+assertEqual(result, "noma")
