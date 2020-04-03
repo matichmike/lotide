54bb3d5636f09c69ef14de3aaed938f36254e46a
Michael Matich <matich.mike@gmail.com>
2020-04-03 00:09:11 +0000

    map function

---
+++ map.js
@@ -0,0 +1,36 @@
+const assertArraysEqual = function(firstArray, secondArray) {
+  if (firstArray.length !== secondArray.length) {
+    console.log(`${firstArray} !== ${secondArray}`);
+  }
+  for (let i = 0; i < firstArray.length; i++) {
+    if (firstArray[i] !== secondArray[i]) {
+      console.log(`${firstArray} !== ${secondArray}`);
+    }
+  }
+  console.log(`${firstArray} === ${secondArray}`);
+}
+
+const eqArrays = function(firstArray, secondArray) {
+  if (firstArray.length !== secondArray.length) {
+    return false;
+  }
+  for (let i = 0; i < firstArray.length; i++) {
+    if (firstArray[i] !== secondArray[i]) {
+      return false;
+    }
+  }
+  return true;
+}
+
+const words = ["ground", "control", "to", "major", "tom"];
+const map = function(array, callback) {
+  const results = [];
+  for (let item of array) {
+    results.push(callback(item));
+  }
+  return results;
+}
+const results1 = map(words, word => word[0]);
+console.log(results1);
+
+assertArraysEqual([ 'g', 'c', 't', 'm', 't' ], [ 'g', 'c', 't', 'm', 't' ]);
