e941d3576439b51ecf1a4160f6675bb6c164d3a8
Michael Matich <matich.mike@gmail.com>
2020-04-06 23:29:52 +0000

    tailTest chai

--- test/tailTest.js
+++ test/tailTest.js
@@ -1,5 +1,8 @@
-const assertEqual = require('../assertEqual');
-const tail = require('../tail');
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-tail(words);
-assertEqual(words.length, 3);
+const assert = require('chai').assert;
+const tail   = require('../tail');
+
+describe("#tail", () => {
+  it("returns 'Labs' for ['Yo Yo', 'Lighthouse', 'Labs']", () => {
+    assert.deepEqual(tail(["Yo Yo", "Lighthouse", "Labs"]), "Labs"); 
+});
+});
