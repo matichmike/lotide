74b6035bf73d3abf8d4a6780cf7e68dfc7b5f752
Michael Matich <matich.mike@gmail.com>
2020-04-06 20:29:33 +0000

    middle dry

--- middle.js
+++ middle.js
@@ -1,27 +1,3 @@
-const eqArrays = function(firstArray, secondArray) {
-  if (firstArray.length !== secondArray.length) {
-    return false;
-  }
-  for (let i = 0; i < firstArray.length; i++) {
-    if (firstArray[i] !== secondArray[i]) {
-      return false;
-    }
-  }
-  return true;
-}
-
-const assertArraysEqual = function(firstArray, secondArray) {
-  if (firstArray.length !== secondArray.length) {
-    console.log(`${firstArray} !== ${secondArray}`);
-  }
-  for (let i = 0; i < firstArray.length; i++) {
-    if (firstArray[i] !== secondArray[i]) {
-      console.log(`${firstArray} !== ${secondArray}`);
-    }
-  }
-  console.log(`${firstArray} === ${secondArray}`);
-}
-
 const middle = function(array) {
   let newArray = [];
   if (array.length <= 2) {
@@ -36,9 +12,5 @@ const middle = function(array) {
   return newArray;
 }
 
-console.log(middle([1])) // => []
-console.log(middle([1, 2])) // => []
-console.log(middle([1, 2, 3])) // => [2]
-console.log(middle([1, 2, 3, 4, 5])) // => [3]
-console.log(middle([1, 2, 3, 4])) // => [2, 3]
-console.log(middle([1, 2, 3, 4, 5, 6])) // => [3, 4]
+module.exports = middle;
+
