26d36005a50e9756ee11a472f44a9d9690db60ab
Michael Matich <matich.mike@gmail.com>
2020-04-06 18:25:07 +0000

    head dry

--- head.js
+++ head.js
@@ -1,12 +1,7 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
-  } else if (actual !== expected) {
-    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
+const assertEqual = require('./assertEqual');
+
 const head = function (actual) {
   return actual[0];
 }
-assertEqual(head([5,6,7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+
+module.exports = head;
